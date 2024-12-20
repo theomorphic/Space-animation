443f4448ba45e6655d9903c967b1d35b96af2de8
theomorphic <FelixMK@yandex.ru>
2024-12-20 21:24:03 +0500

    feat: add skills section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
